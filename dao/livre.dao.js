6869dfd052cada7ea4dc24504debde5ae80116cf
Morgan <richard_morgan@hotmail.fr>
2021-06-09 15:03:38 +0200

    modification de la requête SQL sur la fonction getAllByTitle dans livre.dao

--- dao/livre.dao.js
+++ dao/livre.dao.js
@@ -3,7 +3,7 @@ const connection = require('../database.js');
 
 exports.getAll = () => {
     return new Promise((resolve, reject) => {
-        const req = connection.query("SELECT * FROM livre", (err, result) => {
+        const req = connection.query("SELECT * FROM livre l join article a on a.reference_article = l.reference_article", (err, result) => {
             console.log(req.sql)
             err ? reject(err) : resolve(result);
         });
@@ -13,20 +13,17 @@ exports.getAll = () => {
 
 exports.getOneById = (idLivre) => {
     return new Promise((resolve, reject) => {
-        const req = connection.query("SELECT * FROM livre where idLivre= ?", idLivre,(err, result) => {
+        const req = connection.query("SELECT * FROM livre l join article a on a.reference_article = l.reference_article where idLivre= ?", idLivre,(err, result) => {
             console.log(req.sql)
             err || result.length == 0 ? reject(err) : resolve(result);
         });
     });
 };
 
-
-
-
 exports.getAllByTitle = (titre) => {
     return new Promise ((resolve, rejetc) => {
        let input = '%'.concat(titre.concat('%'));
-        const req = connection.query("SELECT * FROM livre where titre like ?", input, (err, result) => {
+        const req = connection.query("SELECT l.idLivre, l.titre, l.image, g.type, au.nom, au.prenom, ar.prixUnitaire, ar.quantiteEnStock FROM livre l JOIN genre g on l.idGenre = g.idGenre JOIN article ar on l.reference_article = ar.reference_article JOIN livre_auteur la on l.idLivre = la.idLivre JOIN auteur au on la.idAuteur = au.idAuteur WHERE l.titre LIKE ?", input, (err, result) => {
             err || result.affectedRows == 0 ? reject(err) : resolve(result);
         });
     });
@@ -34,7 +31,7 @@ exports.getAllByTitle = (titre) => {
 
 exports.getAllByGenre = (id) => {
     return new Promise ((resolve, reject) => {
-        const req = connection.query("SELECT * FROM livre where idGenre= ?", id, (err, result) => {
+        const req = connection.query("SELECT * FROM livre l join article a on a.reference_article = l.reference_article where idGenre= ?", id, (err, result) => {
             console.log(req.sql)
             err || result.affectedRows == 0 ? reject(err) : resolve(result);
         });
