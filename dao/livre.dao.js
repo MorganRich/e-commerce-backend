10eb2272876ae6c93cec76a7c51856343aed9b97
Morgan <richard_morgan@hotmail.fr>
2021-06-10 12:38:46 +0200

    mise à jour de la requête pour la recherche d'un livre par id

--- dao/livre.dao.js
+++ dao/livre.dao.js
@@ -13,7 +13,7 @@ exports.getAll = () => {
 
 exports.getOneById = (idLivre) => {
     return new Promise((resolve, reject) => {
-        const req = connection.query("SELECT * FROM livre l join article a on a.reference_article = l.reference_article where idLivre= ?", idLivre,(err, result) => {
+        const req = connection.query("SELECT l.idLivre, l.numISBN13, l.titre, l.image, l.format, l.reference_article, g.type, au.nom, au.prenom, ar.resume, ar.prixUnitaire, ar.quantiteEnStock FROM livre l JOIN genre g ON l.idGenre = g.idGenre JOIN article ar ON l.reference_article = ar.reference_article JOIN livre_auteur la ON l.idLivre = la.idLivre JOIN auteur au ON la.idAuteur = au.idAuteur WHERE l.idLivre = ?", idLivre, (err, result) => {
             console.log(req.sql)
             err || result.length == 0 ? reject(err) : resolve(result);
         });
@@ -21,16 +21,16 @@ exports.getOneById = (idLivre) => {
 };
 
 exports.getAllByTitle = (titre) => {
-    return new Promise ((resolve, rejetc) => {
-       let input = '%'.concat(titre.concat('%'));
-        const req = connection.query("SELECT l.idLivre, l.titre, l.image, g.type, au.nom, au.prenom, ar.prixUnitaire, ar.quantiteEnStock FROM livre l JOIN genre g on l.idGenre = g.idGenre JOIN article ar on l.reference_article = ar.reference_article JOIN livre_auteur la on l.idLivre = la.idLivre JOIN auteur au on la.idAuteur = au.idAuteur WHERE l.titre LIKE ?", input, (err, result) => {
+    return new Promise((resolve, rejetc) => {
+        let input = '%'.concat(titre.concat('%'));
+        const req = connection.query("SELECT l.idLivre, l.titre, l.image, g.type, au.nom, au.prenom, ar.prixUnitaire, ar.quantiteEnStock FROM livre l JOIN genre g ON l.idGenre = g.idGenre JOIN article ar ON l.reference_article = ar.reference_article JOIN livre_auteur la ON l.idLivre = la.idLivre JOIN auteur au ON la.idAuteur = au.idAuteur WHERE l.titre LIKE ?", input, (err, result) => {
             err || result.affectedRows == 0 ? reject(err) : resolve(result);
         });
     });
 };
 
 exports.getAllByGenre = (id) => {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const req = connection.query("SELECT * FROM livre l join article a on a.reference_article = l.reference_article where idGenre= ?", id, (err, result) => {
             console.log(req.sql)
             err || result.affectedRows == 0 ? reject(err) : resolve(result);
