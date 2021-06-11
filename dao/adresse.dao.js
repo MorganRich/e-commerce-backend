f5c270e13d0216a92738acbdf213dae21b64e301
Morgan <richard_morgan@hotmail.fr>
2021-06-11 14:09:45 +0200

    modification des requêtes SQL pour la gestion du compte (modification et ajout)

--- dao/adresse.dao.js
+++ dao/adresse.dao.js
@@ -19,27 +19,27 @@ exports.getOneById = (id) => {
 
 exports.add = (a) => {
     return new Promise((resolve, reject) => {
-        const req = connection.query("INSERT INTO adresse SET typeDeVoie = ?,numRue = ?, codePostal = ?, ville = ?, complement=?", [a.typeDeVoie, a.rue, a.codePostal, a.ville, a.complement], (err, result) => {
-                console.log(req.sql)
-                err ? reject(err) : resolve(result);
-            });
+        const req = connection.query("INSERT INTO adresse SET typeDeVoie = ?, numRue = ?, codePostal = ?, ville = ?, complement=?", [a.typeDeVoie, a.numRue, a.codePostal, a.ville, a.complement], (err, result) => {
+            console.log(req.sql)
+            err ? reject(err) : resolve(result);
+        });
     });
 };
+
 exports.edit = (id, a) => {
-   
     return new Promise((resolve, reject) => {
-        const req = connection.query("UPDATE adresse SET ? WHERE idAdresse = ?", [a, id], (err,result) => {
+        const req = connection.query("UPDATE adresse SET ? WHERE idAdresse = ?", [a, id], (err, result) => {
             console.log(req.sql)
             err || result.affectedRows == 0 ? reject(err) : resolve(result);
         });
     });
-
 };
+
 exports.delete = (id) => {
     return new Promise((resolve, reject) => {
         const req = connection.query("DELETE FROM adresse WHERE  idAdresse = ?", id, (err, result) => {
             console.log(req.sql)
-            err || result.affectedRows == 0  ? reject(err) : resolve(result);
+            err || result.affectedRows == 0 ? reject(err) : resolve(result);
         });
     });
 };
--- dao/personneAdresse.dao.js
+++ dao/personneAdresse.dao.js
@@ -4,36 +4,32 @@ exports.getAllAdressesOfPersonne = (idUtilisateur) => {
     return new Promise((resolve, reject) => {
         const req = connection.query("SELECT a.idAdresse, typeDeVoie, numRue, codePostal, ville, complement FROM adresse a JOIN utilisateur_adresse pa ON pa.idAdresse = a.idAdresse  WHERE idUtilisateur = ? ", idUtilisateur, (err, result) => {
             console.log(req.sql)
-            err  ? reject(err) : resolve(result);
+            err ? reject(err) : resolve(result);
         });
     });
 };
 
-
-exports.getOneAdresseOfPersonne =(idUtilisateur, idAdresse) => {
-    return new Promise((resolve, reject ) => {
-        const req = connection.query("SELECT a.idAdresse, typeDeVoie, numRue, codePostal, ville, complement FROM adresse a join utilisateur_adresse pa ON pa.idAdresse = a.idAdresse WHERE idUtilisateur = ? and idAdresse =?", [idUtilisateur, idAdresse],(err, result) => {
+exports.getOneAdresseOfPersonne = (idUtilisateur, idAdresse) => {
+    return new Promise((resolve, reject) => {
+        const req = connection.query("SELECT a.idAdresse, typeDeVoie, numRue, codePostal, ville, complement FROM adresse a join utilisateur_adresse pa ON pa.idAdresse = a.idAdresse WHERE idUtilisateur = ? and idAdresse =?", [idUtilisateur, idAdresse], (err, result) => {
             console.log(req.sql)
             err ? reject(err) : resolve(result);
         });
     });
-
 }
 
-exports.getBillingAdresseOfPersonneByType =(idUtilisateur) => {
-    return new Promise((resolve, reject ) => {
-        const req = connection.query("SELECT a.idAdresse, typeDeVoie, numRue, codePostal, ville, complement FROM adresse a join utilisateur_adresse pa ON pa.idAdresse = a.idAdresse WHERE idUtilisateur =? and idType =2",idUtilisateur,(err, result) => {
+exports.getBillingAdresseOfPersonneByType = (idUtilisateur) => {
+    return new Promise((resolve, reject) => {
+        const req = connection.query("SELECT a.idAdresse, typeDeVoie, numRue, codePostal, ville, complement FROM adresse a join utilisateur_adresse pa ON pa.idAdresse = a.idAdresse WHERE idUtilisateur =? and idType =2", idUtilisateur, (err, result) => {
             console.log(req.sql)
-           
             err ? reject(err) : resolve(result);
         });
     });
-
 }
 
-exports.getTypeOfAdresseOfUtilisateur =(idUtilisateur, idAdresse) => {
-    return new Promise((resolve, reject ) => {
-        const req = connection.query("SELECT t.nom FROM type_adresse t join utilisateur_adresse ua ON ua.idType = t.idType WHERE idUtilisateur = ? and idAdresse =?", [idUtilisateur, idAdresse],(err, result) => {
+exports.getTypeOfAdresseOfUtilisateur = (idUtilisateur, idAdresse) => {
+    return new Promise((resolve, reject) => {
+        const req = connection.query("SELECT t.nom FROM type_adresse t join utilisateur_adresse ua ON ua.idType = t.idType WHERE idUtilisateur = ? and idAdresse =?", [idUtilisateur, idAdresse], (err, result) => {
             console.log(req.sql)
             err ? reject(err) : resolve(result);
         });
@@ -59,8 +55,8 @@ exports.addOneAdresseOfPersonne = (idPersonne, idAdresse, idType) => {
 };
 
 exports.edit = (idAdresse, idPa) => {
-    return new Promise ((resolve, reject) => {
-        const req = connexion.query ("UPDATE utilisateur_adresse pa SET idAdresse=? WHERE  pa.id =? ", [idAdresse,idPa], (err, result) => {
+    return new Promise((resolve, reject) => {
+        const req = connexion.query("UPDATE utilisateur_adresse pa SET idAdresse=? WHERE  pa.id =? ", [idAdresse, idPa], (err, result) => {
             console.log(req.sql)
             err ? reject(err) : resolve(result);
         })
@@ -71,7 +67,7 @@ exports.deleteByIdPersonne = (idUtilisateur) => {
     return new Promise((resolve, reject) => {
         const req = connection.query("DELETE FROM utilisateur_adresse WHERE idUtilisateur = ?", idUtilisateur, (err, result) => {
             console.log(req.sql)
-            err  ? reject(err) : resolve(result);
+            err ? reject(err) : resolve(result);
         });
     });
 };
